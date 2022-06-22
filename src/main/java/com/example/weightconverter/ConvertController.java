2fa652a85f44962aa7f6d216d798a873a2f5321e
Marc Simoneit <wi21b112@technikum-wien.at>
2022-06-22 16:01:46 +0200

    added convertcontroller, ouncetogram function and tests

---
+++ src/main/java/com/example/weightconverter/ConvertController.java
@@ -0,0 +1,20 @@
+package com.example.weightconverter;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConvertController {
+
+        Converter convert = new Converter();
+
+        @RequestMapping("/ed")
+        public double ounce (
+                @RequestParam double ounce) {
+            return convert.ouncetogram(ounce);
+        }
+
+
+
+    }
---
+++ src/main/java/com/example/weightconverter/Converter.java
@@ -0,0 +1,11 @@
+package com.example.weightconverter;
+
+public class Converter {
+    public double ouncetogram(
+            double ounce
+    ) {
+        return ounce * 28.35;
+    }
+}
+
+
